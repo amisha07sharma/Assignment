0b48e85fad9813b6b403820d84242f10f297a374
Amisha <msg.amisha@gmail.com>
2022-01-20 23:44:42 +0530

    [Amisha | Added test case for DELETE API ]

--- backend/main_test.go
+++ backend/main_test.go
@@ -64,3 +64,31 @@ func TestPostMethod(t *testing.T) {
 		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
 	}
 }
+func TestDeleteMethod(t *testing.T) {
+
+	gin.SetMode(gin.TestMode)
+
+	r := gin.Default()
+
+	ConnectDatabase()
+
+	r.DELETE("/deleteperson/:ID", CreatePerson)
+
+	req, err := http.NewRequest(http.MethodPost, "/deleteperson/:ID", nil)
+
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
+	}
+
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	fmt.Println(w.Body)
+
+	if w.Code == http.StatusOK {
+		t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+	} else {
+		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+	}
+}
